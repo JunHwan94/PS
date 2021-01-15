009647bbc60a9ac2fb13678d1a6dc89d7689c0b6
jjun <vkffkehlss@naver.com>
2021-01-15 23:21:32 +0900

    2021.01.15

--- src/baekjoon/문자열/10809.kt
+++ src/baekjoon/문자열/10809.kt
@@ -3,7 +3,7 @@ package baekjoon.문자열
 fun main(){
     val br = System.`in`.bufferedReader()
     val arr = Array(26){ -1 }
-    val a: Int = 'A'.toInt()
+    val a: Int = 'a'.toInt()
     val input = br.readLine()
     for(c in 'a'..'z'){
         for(i in input.indices){
---
+++ src/baekjoon/문자열/1157.kt
@@ -0,0 +1,26 @@
+package baekjoon.문자열
+
+
+fun main(){
+    val arr = Array(26){ 0 }
+    System.`in`.bufferedReader().readLine().onEach { c ->
+        arr[c.toLowerCase() - 'a']++
+    }
+    var idx = 0
+    var max = 0
+    for(i in arr.indices){
+        if(max < arr[i]){
+            idx = i
+            max = arr[i]
+        }
+    }
+    var maxCnt = 0
+    for(i in arr.indices){
+        if(max == arr[i]){
+            maxCnt++
+        }
+    }
+    if(maxCnt > 1){
+        println("?")
+    }else println((idx + 'a'.toInt()).toChar().toUpperCase())
+}
