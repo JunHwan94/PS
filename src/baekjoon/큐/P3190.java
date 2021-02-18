6c02ac7f073610fef6a2e1525c3e0e89b38db381
jjun <vkffkehlss@naver.com>
2021-02-18 21:14:26 +0900

    큐 - 뱀 풀이 추가

--- src/baekjoon/큐/P3190.java
+++ src/baekjoon/큐/P3190.java
@@ -3,41 +3,95 @@ package baekjoon.큐;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class P3190 {
+    static int N;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
-        int[][] field = new int[N][N];
+        N = Integer.parseInt(br.readLine());
         int aCnt = Integer.parseInt(br.readLine());
-        Queue<int[]> aq = new LinkedList<>();
+        List<Pair> apples = new ArrayList<>();
         StringTokenizer token;
         for(int i = 0; i < aCnt; i++){
             token = new StringTokenizer(br.readLine());
-            aq.offer(new int[]{Integer.parseInt(token.nextToken()), Integer.parseInt(token.nextToken())});
+            int y = Integer.parseInt(token.nextToken());
+            int x = Integer.parseInt(token.nextToken());
+            apples.add(new Pair(x, y));
         }
 
         int cCnt = Integer.parseInt(br.readLine());
-        int[] cSec = new int[cCnt];
-        char[] cArr = new char[cCnt];
+        Map<Integer, Character> cMap = new TreeMap<>();
         for(int i = 0; i < cCnt; i++){
             token = new StringTokenizer(br.readLine());
-            cSec[i] = Integer.parseInt(token.nextToken());
-            cArr[i] = token.nextToken().toCharArray()[0];
+            cMap.put(Integer.parseInt(token.nextToken()), token.nextToken().charAt(0));
         }
 
         int sec = 0, dir = 0;
-        int sx = 0, sy = 0;
-        int[] dx = {1, 0, -1, 0}; // 우하좌상
-        int[] dy = {0, 1, 0, -1};
-//        List<>posList
-        while(!aq.isEmpty()){
+        boolean over = false;
+        int[] dx = {1, 0, -1 ,0};
+        int[] dy = {0, 1, 0 ,-1};
+        Deque<Pair> posQueue = new LinkedList<>();
+        posQueue.offerFirst(new Pair(1, 1)); // First : 머리
+        while(true){
+            if(dir == -1) dir = 3;
+            int nx = posQueue.peekFirst().x + dx[dir];
+            int ny = posQueue.peekFirst().y + dy[dir];
+            // 범위 이탈하면 게임 끝
+            if(nx > N || nx < 1 || ny > N || ny < 1){
+                sec++;
+                break;
+            }
+            // 뱀이 다음 위치를 포함하는지 검사
+            // 포함하면 게임 끝
+            for(Pair p : posQueue){
+                if(p.x == nx && p.y == ny){
+                    sec++;
+                    over = true;
+                }
+            }
+            // 포함하지 않으면 머리에 추가
+            if(over) break;
+            else posQueue.offerFirst(new Pair(nx, ny));
 
+            // 사과 먹었는지 검사
+            int aIdx = -1;
+            for(int i = 0; i < apples.size(); i++) {
+                if (apples.get(i).x == nx && apples.get(i).y == ny) {
+                    aIdx = i;
+                    break;
+                }
+            }
+            // 사과 먹었으면 꼬리 유지
+            if(aIdx != -1)
+                apples.remove(aIdx); // 사과 ㅂㅂ
+            else posQueue.pollLast(); // 꼬리 ㅂㅂ
+            sec++;
+
+            // 맨앞 명령어가 지금 실행돼야 하는지
+            int rKey = -1;
+            if(!cMap.isEmpty() && sec == (int)cMap.keySet().toArray()[0]){
+                switch (cMap.get(sec)) {
+                    case 'L': // 왼쪽 방향전환
+                        dir = (dir - 1) % 4;
+                        rKey = sec;
+                        break;
+                    case 'D': // 오른쪽 방향전환
+                        dir = (dir + 1) % 4;
+                        rKey = sec;
+                }
+            }
+            if(rKey != -1) cMap.remove(rKey);
         }
 
         System.out.println(sec);
+    }
+
+    static class Pair{
+        int x, y;
+        Pair(int x, int y){
+            this.x = x;
+            this.y = y;
+        }
     }
 }
--- src/baekjoon/큐/P3190.kt
+++ src/baekjoon/큐/P3190.kt
@@ -1,51 +1,69 @@
 package baekjoon.큐
 
 import java.util.*
+import kotlin.collections.ArrayList
 
 val posQueue = LinkedList<Pair<Int, Int>>().apply { offer(Pair(1, 1)) }
 var N = 0
 val apples = ArrayList<Pair<Int, Int>>()
-val cmds = LinkedList<Pair<Int, Char>>()
+val cmds = LinkedList<Pair<Int, String>>()
 fun main(){
     val br = System.`in`.bufferedReader()
     N = br.readLine().toInt()
 
     repeat(br.readLine().toInt()){
-        val input = br.readLine().split(" ")
-        apples.add(Pair(input[0].toInt(), input[1].toInt()))
+        val input = br.readLine().split(' ')
+        apples.add(Pair(input[1].toInt(), input[0].toInt()))
     }
+
     repeat(br.readLine().toInt()){
-        val input = br.readLine().split(" ")
-        cmds.add(Pair(input[0].toInt(), input[1][0]))
+        val input = br.readLine().split(' ')
+        cmds.offer(Pair(input[0].toInt(), input[1]))
     }
 
     var sec = 0
     var dir = 0
+    var over = false
     // 우하좌상 순
     val dx = intArrayOf(1, 0, -1, 0)
     val dy = intArrayOf(0, 1, 0, -1)
-    while(apples.size > 0){
-        println(apples)
-        val nx = posQueue.peek().first + dx[dir]
-        val ny = posQueue.peek().second + dy[dir]
-        if(nx > N || ny > N || nx < 1 || ny < 1) break
-        posQueue.offer(Pair(nx, ny))
-        var getApple = false
-        for(i in apples.indices){
-            if(apples[i].first == nx && apples[i].second == ny) {
-                apples.remove(apples[i])
-                getApple = true
+    while(true){
+        dir %= 4
+        val nx = posQueue.last().first + dx[dir]
+        val ny = posQueue.last().second + dy[dir]
+        for(p in posQueue.iterator()){
+            if(p.first == nx && p.second == ny){
+                sec++
+                over = true
+                break
             }
         }
+        if(over) break
+        if(nx > N || ny > N || nx < 1 || ny < 1){
+            sec++
+            break
+        }
+        posQueue.offer(Pair(nx, ny))
+        var getApple = false
+        var appleToRemove: Pair<Int, Int>? = null
+        for(i in apples.indices){
+            if(apples[i].first == nx && apples[i].second == ny) {
+                appleToRemove = apples[i]
+                getApple = true
+                break
+            }
+        }
+        apples.remove(appleToRemove)
         if(!getApple) posQueue.poll()
         sec++
 
-        if(cmds.peek().first == sec){
+        if(cmds.isNotEmpty() && cmds.peek().first == sec){
             when(cmds.poll().second){
-                'L' -> dir--
-                'D' -> dir++
+                "L" -> dir--
+                "D" -> dir++
             }
         }
     }
+//    println(dir)
     println(sec)
 }
