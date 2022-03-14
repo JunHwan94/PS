a4aae9a71bebfb7a2dc56e54b1bad24b5ac15959
JunHwanKim <jjun940417@gmail.com>
2022-03-14 22:33:54 +0900

    sk ict 코딩테스트 코드 수정

--- src/test/skbe.java
+++ src/test/skbe.java
@@ -7,58 +7,36 @@ public class skbe {
     }
 
     static int[] values = {1, 5, 10, 50, 100, 500};
-    static int[] arr;
-    static Coin[] coins;
     static int solution(int money, int[] costs){
-        coins = new Coin[6];
+        Queue<Coin> pq = new PriorityQueue<>();
         for (int i = 0; i < 6; i++) {
             coins[i] = new Coin(values[i], costs[i]);
         }
 
-        arr = new int[6];
-        selected = new boolean[6];
-        minCost = 1000000000;
-        perm(0, money);
-
-        return minCost;
-    }
-
-    static boolean[] selected;
-    static int minCost;
-    static void perm(int n, int money){
-        if(n == 6){
-            // 앞에꺼부터 최대한 많은 개수로 생산비용 계산
-            int totalCost = 0;
-            for (int i = 0; i < arr.length; i++) {
-                while(money >= coins[arr[i]].val){
-                    // 가치 값 감소
-                    money -= coins[arr[i]].val;
-                    // 생산 비용 증가
-                    totalCost += coins[arr[i]].cost;
-                }
-            }
-//            System.out.println(totalCost);
-            minCost = Math.min(minCost, totalCost);
-            return;
+        int cost = 0;
+        while(!pq.isEmpty()){
+            Coin coin = pq.poll();
+            int share = money / coin.val;
+            money %= coin.val;
+            cost += share * coin.cost;
         }
 
-        for (int i = 0; i < 6; i++) {
-            if(selected[i]) {
-                continue;
-            }
-            selected[i] = true;
-            arr[n] = i;
-            perm(n + 1, money);
-            selected[i] = false;
-        }
+        return cost;
     }
 
-    static class Coin{
+    static class Coin implements Comparable<Coin>{
         int val, cost;
+        double pivot;
 
         Coin(int val, int cost){
             this.val = val;
             this.cost = cost;
+            this.pivot = (double)val / cost;
+        }
+        
+        @Override
+        public int compareTo(Coin o){
+            return pivot == o.pivot ? o.val - val : Double.compare(o.pivot, pivot);
         }
     }
 }
