fbccd481c418f6e9b469121f752567f830efdfc8
김준환 <vkffkehlss@gmail.com>
2022-04-01 21:10:36 +0900

    여행 가자 풀이

--- src/baekjoon/UnionFind/P1976.java
+++ src/baekjoon/UnionFind/P1976.java
@@ -6,53 +6,77 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class P1976 {
-    static int N, M; // 도시 수, 여행계획에 속한 도시 수(중복가능)
-    static int[] cities;
+
+    static int N, M;
+    static int[] roots, size;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         N = stoi(br.readLine());
         M = stoi(br.readLine());
-        cities = new int[N + 1];
-        make();
+        roots = new int[N + 1];
+        size = new int[N + 1];
+
+        for (int i = 1; i < N + 1; i++) {
+            roots[i] = i;
+            size[i] = 1;
+        }
 
         StringTokenizer st;
-        for(int i = 1; i <= N; i++){
+        for (int i = 1; i < N + 1; i++) {
             st = new StringTokenizer(br.readLine());
-            for(int j = 1; j <= N; j++) {
-                if(stoi(st.nextToken()) == 1)
+            for (int j = 1; j < N + 1; j++) {
+                int connected = stoi(st.nextToken());
+                if(connected == 1) {
                     union(i, j);
+                }
             }
         }
+
+        for (int i = 1; i < N + 1; i++) {
+            System.out.print(roots[i] + " ");
+        }
+
         st = new StringTokenizer(br.readLine());
-        int start = stoi(st.nextToken());
-        for(int i = 1; i < M; i++){
-            int end = stoi(st.nextToken());
-            if(findSet(start) != findSet(end)) { // 연결되어있지 않으면 수행
+        int p = stoi(st.nextToken());
+        int q;
+        while(st.hasMoreTokens()){
+            q = stoi(st.nextToken());
+            if(!check(p, q)){
                 System.out.println("NO");
                 return;
             }
-            start = end;
+            p = q;
         }
+
         System.out.println("YES");
     }
 
-    static void make() {
-        for (int i = 1; i <= N; i++) {
-            cities[i] = i;
+    static void union(int p, int q){
+        int a = find(p);
+        int b = find(q);
+        if(a == b){
+            return;
+        }
+        roots[a] = b;
+        if(size[a] < size[b]){
+            roots[a] = find(b);
+            size[b] += size[a];
+        }else{
+            roots[b] = find(a);
+            size[a] += size[b];
         }
     }
 
-    static int findSet(int a) {
-        if(cities[a] == a) return a;
-        return cities[a] = findSet(cities[a]);
+    static int find(int p){
+        while(roots[p] != p){
+            roots[p] = roots[roots[p]];
+            p = roots[p];
+        }
+        return p;
     }
 
-    static boolean union(int a, int b) {
-        int aRoot = findSet(a);
-        int bRoot = findSet(b);
-        if(aRoot == bRoot) return false;
-        cities[bRoot] = aRoot;
-        return true;
+    static boolean check(int p, int q){
+        return find(p) == find(q);
     }
 
     static int stoi(String s){
